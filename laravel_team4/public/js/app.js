94995640c08248d600642b749d925c65d53435f5
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-08-31 17:34:40 +0200

    add page adv

--- laravel_team4/public/js/app.js
+++ laravel_team4/public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/boolean_php/boolbnb_gitpractice/team4_gitpractice/laravel_team4/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/boolean_php/boolbnb_gitpractice/team4_gitpractice/laravel_team4/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\Esercizi\Esercizi-Laravel\team4\team4_gitpractice\laravel_team4\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\Esercizi\Esercizi-Laravel\team4\team4_gitpractice\laravel_team4\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
